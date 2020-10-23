e212705becc2cc3fbadc5b03d85d59927cd74c69
avenmia <mavend2@gmail.com>
2020-10-22 20:43:46 -0500

    Removing extra code

--- resume.cs
+++ resume.cs
@@ -18,20 +18,7 @@ namespace resume
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-            
             JObject resumeObject = JObject.Parse(File.ReadAllText("./resumeData.json"));
-            Console.WriteLine(resumeObject);
 
             return new OkObjectResult(resumeObject);
         }
